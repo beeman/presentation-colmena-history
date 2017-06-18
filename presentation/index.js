0f204795da659afcae55cb561d7bf403baf07c88
Bram Borggreve <borggreve@gmail.com>
2017-06-18 06:05:11 -0500

    Initial sketch of Presentation

--- presentation/index.js
+++ presentation/index.js
@@ -50,34 +50,113 @@ export default class Presentation extends React.Component {
       <Deck transition={["zoom", "slide"]} transitionDuration={500} theme={theme}>
         <Slide transition={["zoom"]} bgColor="primary">
           <Heading size={1} fit caps lineHeight={1} textColor="secondary">
-            Spectacle Boilerplate
+            colmena
           </Heading>
+          <Text margin="10px 0 0" textColor="secondary" size={6} fit bold>
+            <code>kolˈmena</code>
+          </Text>
           <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
-            open the presentation/index.js file to get started
+            Spanish for beehive
           </Text>
         </Slide>
         <Slide transition={["fade"]} bgColor="tertiary">
-          <Heading size={6} textColor="primary" caps>Typography</Heading>
-          <Heading size={1} textColor="secondary">Heading 1</Heading>
-          <Heading size={2} textColor="secondary">Heading 2</Heading>
-          <Heading size={3} textColor="secondary">Heading 3</Heading>
-          <Heading size={4} textColor="secondary">Heading 4</Heading>
-          <Heading size={5} textColor="secondary">Heading 5</Heading>
-          <Text size={6} textColor="secondary">Standard text</Text>
+          <Heading size={2} textColor="secondary">History</Heading>
+          <Heading margin="30px 0 0" size={2} textColor="secondary">Current</Heading>
+          <Heading margin="30px 0 0" size={2} textColor="secondary">Future</Heading>
         </Slide>
-        <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
-          <Heading size={6} textColor="secondary" caps>Standard List</Heading>
+
+
+        <Slide transition={["fade"]} bgColor="tertiary">
+          <Heading size={1} textColor="secondary">History</Heading>
+          <Text margin="30px 0" size={6} textColor="secondary">Why AngularJS (1.2.x)</Text>
           <List>
-            <ListItem>Item 1</ListItem>
-            <ListItem>Item 2</ListItem>
-            <ListItem>Item 3</ListItem>
-            <ListItem>Item 4</ListItem>
+            <ListItem>Batteries included</ListItem>
+            <ListItem>Ionic is built on it.</ListItem>
+            <ListItem>Great AngularJS SDK for LoopBack</ListItem>
+          </List>
+
+          <Text margin="30px 0" size={6} textColor="secondary">Why LoopBack (2.x)</Text>
+          <List>
+            <ListItem>Rapid API development</ListItem>
+            <ListItem>Easy to extend, integrate</ListItem>
+            <ListItem>Running on NodeJS</ListItem>
           </List>
         </Slide>
+
+
+        <Slide transition={["fade"]} bgColor="tertiary">
+          <Heading size={1} textColor="secondary">History</Heading>
+
+          <Heading margin="30px 0" size={6} textColor="secondary">Aug, 1st: LoopBack Angular Admin</Heading>
+          <Text margin="30px 0" size={4} textColor="secondary">In need for a Mobile Backend to start building Ionic apps</Text>
+          <Text size={4} textColor="secondary">Proof of Concept of building a CMS using LoopBack and Angular</Text>
+
+          <Heading margin="30px 0" size={6} textColor="secondary">Aug, 22st: First issue/PR</Heading>
+          <Text size={4} textColor="secondary">Project got noticed by the community</Text>
+        </Slide>
+
+        <Slide transition={["fade"]} bgColor="tertiary">
+          <Heading size={1} textColor="secondary">History</Heading>
+
+          <Heading margin="30px 0" size={6} textColor="secondary">Jan, '15: Translation support</Heading>
+          <Text margin="20px 0" size={4} textColor="secondary">Several languages added by community members</Text>
+
+          <Heading margin="30px 0" size={6} textColor="secondary">May, '15: Started a new job</Heading>
+          <Text margin="20px 0" size={4} textColor="secondary">Full-time LoopBack/Angular developer</Text>
+          <Text size={4} textColor="secondary">Thanks to the project!</Text>
+
+          <Heading margin="30px 0" size={6} textColor="secondary">2016: Modern AngularJS (components)</Heading>
+          <Heading margin="30px 0" size={6} textColor="secondary">2016: [unreleased] WebPack + ES6</Heading>
+
+        </Slide>
+        <Slide transition={["fade"]} bgColor="tertiary">
+          <Heading size={1} textColor="secondary">History</Heading>
+
+          <Heading margin="30px 0" size={6} textColor="secondary">Feb, '17: Rewrite in Angular (2.x)</Heading>
+          <Text margin="30px 0" size={4} textColor="secondary">Thanks to Angular CLI</Text>
+          <Text size={4} textColor="secondary">Less functionality on a better basis</Text>
+
+          <Heading margin="30px 0" size={6} textColor="secondary">Feb, '17: Rename to Colmena CMS</Heading>
+          <Text size={4} textColor="secondary">Move to GitHub org colmena</Text>
+
+          <Heading margin="30px 0" size={6} textColor="secondary">May, '17: Move to lerna</Heading>
+          <Text size={4} textColor="secondary">Apps/Modules/Packages all the way</Text>
+        </Slide>
+
+
+        <Slide transition={["fade"]} bgColor="tertiary">
+          <Heading size={1} textColor="secondary">Current</Heading>
+
+          <Heading margin="30px 0" size={6} textColor="secondary">Full time working on Colmena.</Heading>
+
+
+          <Text size={4} textColor="secondary">Focus-shift from CMS to a platform to enable Rapid API Development.</Text>
+
+        </Slide>
+        <Slide transition={["fade"]} bgColor="tertiary">
+          <Heading size={1} textColor="secondary">Current</Heading>
+
+        </Slide>
+        <Slide transition={["fade"]} bgColor="tertiary">
+          <Heading size={1} textColor="secondary">Current</Heading>
+
+        </Slide>
+
+
+        <Slide transition={["fade"]} bgColor="tertiary">
+          <Heading size={1} textColor="secondary">Future</Heading>
+          <List>
+            <ListItem>Work towards initial release</ListItem>
+            <ListItem>Validate the product</ListItem>
+            <ListItem>Create strategy</ListItem>
+          </List>
+        </Slide>
+
         <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
           <BlockQuote>
-            <Quote>Example Quote</Quote>
-            <Cite>Author</Cite>
+            <Quote>Questions?</Quote>
+            <Cite>Twitter: @beeman_nl</Cite>
+            <Cite>GitHub: @beeman</Cite>
           </BlockQuote>
         </Slide>
       </Deck>
